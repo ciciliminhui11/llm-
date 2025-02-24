12b078a804bf6b97ffd87f27e0e04ab1093cb094
water1is <368385061@qq.com>
2025-02-24 23:34:02 +0800

    测试（心累

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import home from '../views/home.vue';  // 导入 Home 页面
 import chat_1 from '../views/TheSearch.vue';
 import chat_2 from '../views/chat_2.vue';
 import chat_3 from '../views/chat_3.vue';
 
 const routes = [
+  
   { path: '/',component: home },
   { path: '/chat_1', component: chat_1 },
   { path: '/chat_2', component: chat_2 },
@@ -12,7 +13,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 });
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    assetsDir: 'static',
-    parallel: false,
-    publicPath: './',
-}
+  publicPath: './', // 使用相对路径
+  outputDir: 'dist', // 构建输出目录
+  assetsDir: 'static', // 静态资源目录
+  indexPath: 'index.html', // 生成的 index.html 文件
+};
